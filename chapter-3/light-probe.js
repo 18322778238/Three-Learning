5e443b69e8cad90d701e0054cbc43ff0840ba9af
Read-Search-Ask <m18322778238@163.com>
2025-11-11 10:29:36 +0800

    生成光探针文件

---
+++ chapter-3/light-probe.js
@@ -0,0 +1,70 @@
+// 光探针
+import * as THREE from 'three'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { initScene } from '../bootstrap/bootstrap.js'
+import { LightProbeGenerator } from 'three/examples/jsm/lights/LightProbeGenerator'
+import { stats } from '../util/stats'
+import { visitChildren } from '../util/modelUtil.js'
+
+const props = {
+  backgroundColor: 0xcccccc,
+  disableLights: true
+}
+const loadIsland = (scene) => {
+  const loader = new GLTFLoader()
+  loader.load('/assets/gltf/flying_island/scene.gltf', (loadedObject) => {
+    // the nested
+    const loadedScene = loadedObject.scene.children[0].children[0].children[0]
+    visitChildren(loadedScene, (c) => {
+      c.receiveShadow = true
+      c.castShadow = true
+    })
+    loadedScene.scale.set(0.022, 0.022, 0.022)
+    loadedScene.translateY(-7)
+    scene.add(loadedScene)
+  })
+}
+
+const loadCubeMap = (renderer, scene) => {
+  const base = 'drachenfels'
+  const ext = 'png'
+  const urls = [
+    '/assets/panorama/' + base + '/posx.' + ext,
+    '/assets/panorama/' + base + '/negx.' + ext,
+    '/assets/panorama/' + base + '/posy.' + ext,
+    '/assets/panorama/' + base + '/negy.' + ext,
+    '/assets/panorama/' + base + '/posz.' + ext,
+    '/assets/panorama/' + base + '/negz.' + ext
+  ]
+
+  new THREE.CubeTextureLoader().load(urls, function (cubeTexture) {
+    cubeTexture.encoding = THREE.sRGBEncoding                 // 设置颜色编码
+    scene.background = cubeTexture                            // 设置为场景背景
+    // 从立方体贴图生成光探针
+    const lp = LightProbeGenerator.fromCubeTexture(cubeTexture)
+    lp.intensity = 15               // 增强光照强度
+    scene.add(lp)                   // 添加到场景
+  })
+}
+
+initScene(props)(({ scene, camera, renderer, orbitControls }) => {
+  camera.position.set(-7, 2, 5)
+  orbitControls.update()
+
+  loadIsland(scene)
+
+  loadCubeMap(renderer, scene)
+
+  const lightProbe = new THREE.LightProbe()
+  scene.add(lightProbe)
+
+  function animate() {
+    requestAnimationFrame(animate)
+    renderer.render(scene, camera)
+    stats.update()
+
+    orbitControls.update()
+  }
+
+  animate()
+})

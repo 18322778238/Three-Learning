d5baf4a68e4f03200864c9c6eb14479b4a99c9c1
Read-Search-Ask <m18322778238@163.com>
2025-11-12 18:58:09 +0800

    汉化相机控制器

--- controls/camera-controls.js
+++ controls/camera-controls.js
@@ -1,9 +1,8 @@
 import * as THREE from 'three'
 
 // 视角名称
-const perspectiveName = 'Perspective Camera'
-// 正交名称
-const orthoName = 'Orthographic Camera'
+const perspectiveName = '透视相机'
+const orthoName = '正交相机'
 
 // TODO: check the lookat
 // 视线方向配置函数:返回一个包含默认视线目标点坐标的对象,用于控制相机注视的目标位置
@@ -30,19 +29,19 @@ export const initializePerspectiveCameraControls = (camera, gui, orbitControls, 
     // 添加各种参数控制器
     const cameraFolder = gui.addFolder(perspectiveName)
     // 视野角度，范围0-180，步长1
-    const fovCtrl = cameraFolder.add(props, 'fov', 0, 180, 1).onChange(updateCamera) 
+    const fovCtrl = cameraFolder.add(props, 'fov', 0, 180, 1).name('视野角度').onChange(updateCamera) 
     // 宽高比，范围0-10，步长0.1
-    const aspectCtrl = cameraFolder.add(props, 'aspect', 0, 10, 0.1).onChange(updateCamera)              
+    const aspectCtrl = cameraFolder.add(props, 'aspect', 0, 10, 0.1).name('宽高比').onChange(updateCamera)              
     // 近裁剪面，范围0-20，步长0.1
-    const nearCtrl = cameraFolder.add(props, 'near', 0, 20, 0.1).onChange(updateCamera) 
+    const nearCtrl = cameraFolder.add(props, 'near', 0, 20, 0.1).name('近裁剪面').onChange(updateCamera) 
     // 远裁剪面，范围5-100，步长0.1                
-    const farCtrl = cameraFolder.add(props, 'far', 5, 100, 0.1).onChange(updateCamera)
+    const farCtrl = cameraFolder.add(props, 'far', 5, 100, 0.1).name('远裁剪面').onChange(updateCamera)
     // 缩放，范围-1到10，步长0.1                 
-    const zoomCtrl = cameraFolder.add(props, 'zoom', 0.1, 10, 0.1).onChange(updateCamera)                
+    const zoomCtrl = cameraFolder.add(props, 'zoom', 0.1, 10, 0.1).name('缩放级别').onChange(updateCamera)                
 
-    cameraFolder.add(vectorProps, 'lookAtX', -10, 10, 0.1).onChange(updateLookAt)
-    cameraFolder.add(vectorProps, 'lookAtY', -10, 10, 0.1).onChange(updateLookAt)
-    cameraFolder.add(vectorProps, 'lookAtZ', -10, 10, 0.1).onChange(updateLookAt)
+    cameraFolder.add(vectorProps, 'lookAtX', -10, 10, 0.1).name('目标点 X').onChange(updateLookAt)
+    cameraFolder.add(vectorProps, 'lookAtY', -10, 10, 0.1).name('目标点 Y').onChange(updateLookAt)
+    cameraFolder.add(vectorProps, 'lookAtZ', -10, 10, 0.1).name('目标点 Z').onChange(updateLookAt)
 
     function updateCamera() {
         camera.fov = props.fov
@@ -79,17 +78,17 @@ export const initializeOrthographicCameraControls = (camera, gui, orbitControls,
     removeIfPresent(gui, orthoName)
 
     const cameraFolder = gui.addFolder(orthoName)
-    cameraFolder.add(props, 'left', -400, -10, 1).onChange(updateCamera)
-    cameraFolder.add(props, 'right', 10, 400, 1).onChange(updateCamera)
-    cameraFolder.add(props, 'top', 0, 200, 1).onChange(updateCamera)
-    cameraFolder.add(props, 'bottom', -200, 0, 1).onChange(updateCamera)
-    cameraFolder.add(props, 'near', 0.1, 10, 0.1).onChange(updateCamera) // 修正near范围
-    cameraFolder.add(props, 'far', 1, 100, 1).onChange(updateCamera)
-    cameraFolder.add(props, 'zoom', 0.1, 10, 0.1).onChange(updateCamera) // 修正zoom范围
+    cameraFolder.add(props, 'left', -400, -10, 1).name('左裁剪面').onChange(updateCamera)
+    cameraFolder.add(props, 'right', 10, 400, 1).name('右裁剪面').onChange(updateCamera)
+    cameraFolder.add(props, 'top', 0, 200, 1).name('上裁剪面').onChange(updateCamera)
+    cameraFolder.add(props, 'bottom', -200, 0, 1).name('下裁剪面').onChange(updateCamera)
+    cameraFolder.add(props, 'near', 0.1, 10, 0.1).name('近裁剪面').onChange(updateCamera) // 修正near范围
+    cameraFolder.add(props, 'far', 1, 100, 1).name('远裁剪面').onChange(updateCamera)
+    cameraFolder.add(props, 'zoom', 0.1, 10, 0.1).name('缩放级别').onChange(updateCamera) // 修正zoom范围
     
-    cameraFolder.add(vectorProps, 'lookAtX', -10, 10, 0.1).onChange(updateLookAt)
-    cameraFolder.add(vectorProps, 'lookAtY', -10, 10, 0.1).onChange(updateLookAt)
-    cameraFolder.add(vectorProps, 'lookAtZ', -10, 10, 0.1).onChange(updateLookAt)
+    cameraFolder.add(vectorProps, 'lookAtX', -10, 10, 0.1).name('目标点 X').onChange(updateLookAt)
+    cameraFolder.add(vectorProps, 'lookAtY', -10, 10, 0.1).name('目标点 Y').onChange(updateLookAt)
+    cameraFolder.add(vectorProps, 'lookAtZ', -10, 10, 0.1).name('目标点 Z').onChange(updateLookAt)
 
     // 变化监听和更新
     function updateCamera() {

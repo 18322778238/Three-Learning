88413eff540a239b0279c8031867fc86b31b2477
Read-Search-Ask <m18322778238@163.com>
2025-11-17 06:58:01 +0800

    3D 几何体边缘线框的可视化工具

---
+++ chapter-6/edges-geometry.js
@@ -0,0 +1,29 @@
+import { bootstrapGeometryScene } from './util/standard-scene'
+import { updateMesh } from './util'
+import * as THREE from 'three'
+import { RoundedBoxGeometry } from 'three/examples/jsm/geometries/RoundedBoxGeometry'
+
+const props = {
+  thresholdAngle: 1 // 边缘检测的角度阈值（单位：度）
+}
+
+// const baseGeometry = new THREE.TorusKnotBufferGeometry(3, 1, 100, 20, 6, 9)
+// const baseGeometry = new THREE.BoxGeometry(3, 3, 3)
+// 基础几何体 - 圆角立方体
+const baseGeometry = new RoundedBoxGeometry(3, 3, 3, 10, 0.4)
+
+const updateGeometry = ({ thresholdAngle }) => {
+  return new THREE.EdgesGeometry(baseGeometry, thresholdAngle)
+}
+
+const geometry = updateGeometry(props)
+
+bootstrapGeometryScene({
+  geometry,
+  provideGui: (gui, mesh) => {
+    const folder = gui.addFolder('THREE.EdgesGeometry')
+    folder.add(props, 'thresholdAngle', 0, 3, 0.01).onChange(() => updateMesh(mesh, updateGeometry(props)))
+  },
+  overrideMaterial: new THREE.LineBasicMaterial({ color: 0x000000 }),
+  useLine: true
+}).then()

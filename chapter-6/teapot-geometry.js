4041a9f3c31f36d493ba5c719c40c7b1c8644c3b
Read-Search-Ask <m18322778238@163.com>
2025-11-17 07:03:20 +0800

    犹他茶壶(Utah Teapot) 3D模型生成器

---
+++ chapter-6/teapot-geometry.js
@@ -0,0 +1,34 @@
+// 犹他茶壶(Utah Teapot) 3D模型生成器
+import { bootstrapGeometryScene } from './util/standard-scene'
+import { updateMesh } from './util'
+import { TeapotGeometry } from 'three/examples/jsm/geometries/TeapotGeometry'
+
+const props = {
+  size: 1,
+  segments: 20,
+  bottom: true,
+  lid: true,
+  body: true,
+  fitLid: true,
+  blinn: true // aspects
+}
+
+const updateGeometry = ({ size, segments, bottom, lid, body, fitLid, blinn }) => {
+  return new TeapotGeometry(size, segments, bottom, lid, body, fitLid, blinn)
+}
+
+const geometry = updateGeometry(props)
+
+bootstrapGeometryScene({
+  geometry,
+  provideGui: (gui, mesh) => {
+    const folder = gui.addFolder('THREE.TeapotGeometry')
+    folder.add(props, 'size', 0, 10, 0.01).onChange(() => updateMesh(mesh, updateGeometry(props)))
+    folder.add(props, 'segments', 1, 30, 1).onChange(() => updateMesh(mesh, updateGeometry(props)))
+    folder.add(props, 'bottom', 0, 10, 0.01).onChange(() => updateMesh(mesh, updateGeometry(props)))
+    folder.add(props, 'lid', 1, 20, 1).onChange(() => updateMesh(mesh, updateGeometry(props)))
+    folder.add(props, 'body', 0, 3, 0.01).onChange(() => updateMesh(mesh, updateGeometry(props)))
+    folder.add(props, 'fitLid', 0, 3, 0.01).onChange(() => updateMesh(mesh, updateGeometry(props)))
+    folder.add(props, 'blinn', 0, 3, 0.01).onChange(() => updateMesh(mesh, updateGeometry(props)))
+  }
+}).then()
